ca157144786d235e73d2389ec8393ac3db705682
Amtresu <conor.follette@gmail.com>
2022-08-10 23:57:14 +0900

    added arrow button functionality

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import _, { update } from 'lodash'
 import './style.css'
 import arrow from '../src/assets/images/add-item-img.png'
 import refresh from '../src/assets/images/refresh.png'
-import { display, addToDo } from './CRUD.js'
+import { display, addToDo} from './CRUD.js'
 
 document.querySelector('.refresh').src = `${refresh}`
 document.querySelector('.arrow').src = `${arrow}`
 
 const form = document.querySelector('.form')
 const input = document.querySelector('.add-item')
+const arrowImg = document.querySelector('.arrow')
 
 window.addEventListener('load', () => {
   const toDoList = document.querySelector('.to-do-list-ul')
@@ -17,6 +18,15 @@ window.addEventListener('load', () => {
 })
 
 form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const toDoList = document.querySelector('.to-do-list-ul')
+  const inputVal = input.value
+  addToDo(inputVal)
+  input.value = ''
+  display(toDoList)
+})
+
+arrowImg.addEventListener('click', (e) => {
   e.preventDefault()
   const toDoList = document.querySelector('.to-do-list-ul')
   const inputVal = input.value
