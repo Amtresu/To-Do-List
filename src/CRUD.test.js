0d43541d49bf23792d88837a161c71d5e9d20217
Amtresu <conor.follette@gmail.com>
2022-08-17 23:32:04 +0900

    making changes to remove function

--- src/CRUD.test.js
+++ src/CRUD.test.js
@@ -1,30 +1,17 @@
 /* eslint-disable */
-import { display, addToDo, clear, clearAll, removeToDo} from './CRUD.js'
+import { add, remove } from 'lodash'
+import { display, addToDo, clear, clearAll, removeToDo, markCompleted} from './CRUD.js'
 
 describe('Testing the add toDo function', () =>{
     test('add item to array of items', () => {
         const storage = JSON.parse(localStorage.getItem('to-do-item'))
         const addItems = addToDo(storage)
-
         expect(addItems).toEqual([{
             description: null,
             completed: false,
             index: 1
         }])
     })
-
-    test('removing items', () => {
-      const storage = JSON.parse(localStorage.getItem('to-do-item'));
-
-      addToDo(storage, '1')
-      addToDo (storage, '2')
-      addToDo (storage, '3')
-
-      const removeItems = removeToDo(2)
-      expect(removeItems).toEqual(2)
-    })
- 
-
     test('testing local storage', () => {
         const obj = {
           description: 'value',
@@ -42,11 +29,16 @@ describe('Testing the add toDo function', () =>{
     })
 
 describe('testing remove function', () => {
-    test('clear all method', () => {
-        const toDoList = document.querySelector('.to-do-list-ul')
-        const storage = localStorage.getItem('to-do-item')
-        const clear = clearAll
-        expect(storage)
-    })
+  test('removing items', () => {
+    const storage = JSON.parse(localStorage.getItem('to-do-item'));
+
+    addToDo(storage, '1')
+    addToDo (storage, '2')
+    addToDo (storage, '3')
+
+    const removeItems = removeToDo(2)
+    expect(removeItems).toEqual(2)
+  })
 })
+
     
